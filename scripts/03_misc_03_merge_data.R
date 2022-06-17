d497b876fac44b4f8865e0e66e759c4fa252ef70
Stefan Munnes <munnes@wzb.eu>
2022-06-17 13:55:38 +0200

    add variables, keep all prize obs, clean script

--- scripts/03_misc_03_merge_data.R
+++ scripts/03_misc_03_merge_data.R
@@ -26,12 +26,12 @@ nominees <- nominees_pt |>
   # 1. add sentiment and topics
   full_join(sent_topics, by = "url_book", suffix = c("_pt", "_st")) |>
   mutate(no_prize = is.na(prize),
-         no_topic = is.na(senti_mean)) |>
+         no_senttop = is.na(senti_mean)) |>
 
   # 12x no prize with coded sent/topics
   # 43x no sent/topics (2x with pt-revs, 41 without pt-revs)
 
-  filter(!no_prize & !no_topic) |> 
+  filter(!no_prize) |>
 
   
   # 2. add gender share of jury
@@ -50,12 +50,14 @@ nominees <- nominees_pt |>
 
   
   # 4. number of books before nomination (by list of DNB books)
-  left_join(dnb_books, by = "url_name") |>
+  full_join(dnb_books, by = "url_name") |>
   group_by(url_name, prize, ynom) |>
   mutate(books_dnb_n = n(),
          books_dnb_prev = sum(year_dnb < ynom),
          no_dnb = is.na(title_dnb)) |>
+    
   filter(row_number() == 1) |>
+    
 
   # 5. times of nomination and previous nominations for each author and prize
   group_by(url_name, prize) |>
@@ -67,10 +69,11 @@ nominees <- nominees_pt |>
   select(authors, title_pt, subti, prize, ynom, shortlist, winner, debut, 
          jury_fem, nom_prize_n, nom_prize_prev,
          ybirth, ydeath, age_nom, female, language, academic, institute,
-         revs_n, revs_fem, revs_n_nomis_st, senti_mean, books_dnb_n, books_dnb_prev,
+         revs_n, revs_fem, revs_n_nomis_st, senti_mean,
+         books_dnb_n, books_dnb_prev, books_pt_n,
          ypub, publisher, pub_place, starts_with("pub_rep"), # pages, price, type, isbn,
          tags, tpcs, keyword_dnb, starts_with("topic_"), topics_orig,
-         url_name, url_book, match_id)
+         url_name, url_book, match_id, no_pt, no_senttop)
 
 
 saveRDS(nominees, file = "../data/nominees.RDS")
@@ -78,18 +81,7 @@ saveRDS(nominees, file = "../data/nominees.RDS")
 
 # SM: add dummy debüt + interaction (?)
 
-
 # SM: scrape Wikipedia:
 # https://de.wikipedia.org/wiki/Liste_deutscher_Literaturpreise
 # https://de.wikipedia.org/wiki/Liste_der_%C3%B6sterreichischen_Literaturpreise
 # https://de.wikipedia.org/wiki/Liste_Schweizer_Literaturpreise
-
-# SM: topic modelling reviews
-
-
-# SM: lastname classification for migration background
-authors_surnames <- select(prizes_df, name, language) |>
-  unique()
-
-# https://forebears.io/surnames/geltinger
-# https://gender-api.com/en/api-docs/v2/query-by-full-name
