fc970e39865f57d00dd69d78cf7947228ae6f96d
Stefan Munnes <munnes@wzb.eu>
2022-08-26 10:46:11 +0200

    new structure, master and rename

---
+++ scripts/03_wp_prizes_all_00_master.R
@@ -0,0 +1,12 @@
+
+# 1. load functions necessary for all later scripts
+source("03_wp_prizes_all_01_func.R")
+
+# 2. scrape htmls of all german speaking prizes from wikipedia
+source("03_wp_prizes_all_01_func.R")
+
+# 3. extracting winners (and nominees) wiki-urls from raw htmls
+source("03_wp_prizes_all_03_extr.R")
+
+# 4. clean list of authors urls and merge with given list of nominees wiki urls
+source("03_wp_prizes_all_04_clean.R")
