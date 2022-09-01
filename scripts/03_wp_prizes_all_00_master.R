37d36772a6ed42bcd3f195c0d9c7bcb25c30f2f4
Stefan Munnes <munnes@wzb.eu>
2022-09-01 09:34:14 +0200

    UTF-8 to uppercase

--- scripts/03_wp_prizes_all_00_master.R
+++ scripts/03_wp_prizes_all_00_master.R
@@ -1,12 +1,12 @@
 
 # 1. load functions necessary for all later scripts
-source("03_wp_prizes_all_01_func.R", encoding = "utf-8")
+source("03_wp_prizes_all_01_func.R", encoding = "UTF-8")
 
 # 2. scrape htmls of all german speaking prizes from wikipedia
-source("03_wp_prizes_all_02_htmls.R", encoding = "utf-8")
+source("03_wp_prizes_all_02_htmls.R", encoding = "UTF-8")
 
 # 3. extracting winners (and nominees) wiki-urls from raw htmls
-source("03_wp_prizes_all_03_extr.R", encoding = "utf-8")
+source("03_wp_prizes_all_03_extr.R", encoding = "UTF-8")
 
 # 4. clean list of authors urls and merge with given list of nominees wiki urls
-source("03_wp_prizes_all_04_clean.R", encoding = "utf-8")
+source("03_wp_prizes_all_04_clean.R", encoding = "UTF-8")
