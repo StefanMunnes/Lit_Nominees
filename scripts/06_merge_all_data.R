f8bdecc6af14425f18f61eeeef6b58b79a4d6174
Stefan Munnes <munnes@wzb.eu>
2023-01-26 17:37:21 +0100

    add last missing topics and poetry

--- scripts/06_merge_all_data.R
+++ scripts/06_merge_all_data.R
@@ -9,9 +9,13 @@ sent_topics <- readRDS("../data/sentiment_topics.RDS") |>
   distinct(across(url_book:topics_orig))
 
 # missing topics
+codes_last_miss  <- read_xlsx("../data/topics_last_miss.xlsx") |>
+  pivot_wider(names_from = "value")
+
 codes_mi <- read.csv("../data/topics_hc_mi.csv", encoding = "UTF-8") |>
   select(match_id, value) |>
-  pivot_wider(names_from = "value")
+  pivot_wider(names_from = "value") |>
+  bind_rows(codes_last_miss)
 
 # Publisher Information
 publisher <- read.csv("../data/publisherstatus/publisherstatus_new.csv",
@@ -141,7 +145,7 @@ nominees <- nominees_pt |>
     ypub, publisher, pub_place, starts_with("pub_rep"),
     starts_with("topic_"), topics_orig, tags, tpcs, keyword_dnb,
     shortlist_date, longlist_date, wv_interval,
-    url_name, url_book, match_id, no_pt, no_senttop
+    url_name, url_book, match_id, no_pt, no_senttop, poetry
   )
   
 saveRDS(nominees, file = "../data/nominees.RDS")
