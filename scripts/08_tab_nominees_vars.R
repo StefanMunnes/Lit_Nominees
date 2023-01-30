5c83b845946d4821b66d513b035531098efe10df
Stefan Munnes <munnes@wzb.eu>
2023-01-30 17:34:16 +0100

    removed creation of categorial vars from metric

--- scripts/08_tab_nominees_vars.R
+++ scripts/08_tab_nominees_vars.R
@@ -4,44 +4,35 @@ p_load("arsenal")
 # load data
 nominees <- readRDS("../data/nominees_rec.RDS")
 
-nom_test <- mutate(nominees,
+noms_all <- mutate(nominees,
   winner = "All Nominees"
 )
 
 # prep different data.frames for different tables
-nominees_all <- nominees |>
-  mutate(winner = FALSE) |>
-  bind_rows(nominees[nominees$winner, ]) |>
-  mutate(winner = ifelse(winner, "Winner", "All Nominees"))
-
-noms_cat <- nom_test |>
-  mutate(across(
-    c(revs_n, senti_mean, senti_vari, wv_mean, homophily),
-    ~ case_when(
-      is.na(.x) ~ "none",
-      .x < mean(.x, na.rm = TRUE)
-      ~ sprintf("< mean (%.1f)", mean(.x, na.rm = TRUE)),
-      .x > mean(.x, na.rm = TRUE)
-      ~ sprintf("> mean (%.1f)", mean(.x, na.rm = TRUE)),
-    ) |>
-      as.ordered()
-  ))
+# nominees_all <- nominees |>
+#   mutate(winner = FALSE) |>
+#   bind_rows(nominees[nominees$winner, ]) |>
+#   mutate(winner = ifelse(winner, "Winner", "All Nominees"))
 
 
 # prep varlist and table options
-varlist <- winner ~ revs_n + senti_mean + senti_vari + homophily + wv_mean +
+varlist <- winner ~ revs_n_cat + senti_mean_cat + senti_vari_cat + wv_mean_cat +
   pub_reputation + prevbooks + prevprize + nom_prize_prev +
   topic_history + topic_politics + topic_relations + topic_identity +
   topic_culture +
-  female + language_german + debut + nonfiction
+  female + homophily + language_german + debut + nonfiction
 
 
-tableby(varlist, data = noms_cat, total = FALSE, cat.simplify = TRUE) |>
+tableby(varlist, data = noms_all, total = FALSE, cat.simplify = TRUE) |>
   set_labels(list(
-    revs_n = "# Reviews", senti_mean = "Review sentiment",
-    senti_vari = "Variance sentiment", wv_mean = "Wikipedia Views",
-    pub_reputation = "Publisher Reputation", prevbooks = "# previous books",
-    prevprize = "# previous prizes", nom_prize_prev = "# previous nominations",
+    revs_n_cat = "# Reviews",
+    senti_mean_cat = "Review sentiment",
+    senti_vari_cat = "Variance sentiment",
+    wv_mean_cat = "Wikipedia Views",
+    pub_reputation = "Publisher Reputation",
+    prevbooks = "# previous books",
+    prevprize = "# previous prizes",
+    nom_prize_prev = "# previous nominations",
     topic_history = "Topic: History",
     topic_politics = "Topic: Politics",
     topic_relations = "Topic: Relations",
@@ -59,6 +50,6 @@ tableby(varlist, data = noms_cat, total = FALSE, cat.simplify = TRUE) |>
   )
 
 
-c <- tableby(varlist, data = nominees_cat, control = controls, cat.simplify = TRUE)
+c <- tableby(varlist, data = noms_all, control = controls, cat.simplify = TRUE)
 
 summary(c, text = TRUE)
