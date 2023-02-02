6bb6066d8ee2f6eec81a65c83d2f238a1860f207
Stefan Munnes <munnes@wzb.eu>
2023-02-03 00:17:15 +0100

    add new mean cat vars and labels

--- scripts/08_tab_nominees_vars.R
+++ scripts/08_tab_nominees_vars.R
@@ -1,27 +1,19 @@
 p_load("arsenal")
 
 
-# load data
-nominees <- readRDS("../data/nominees_rec.RDS")
+# load data and category of by-varialbe to all observations
+noms_all <- readRDS("../data/nominees_rec.RDS") |>
+  mutate(winner = "All Nominations")
 
-# prep by-variable and labels for categories
-noms_all <- nominees |>
-  mutate(
-    winner = "All Nominations",
-    senti_mean_cat = factor(senti_mean_cat,
-      levels = c("none", levels(nominees$senti_mean_cat)[1:2])
-    ),
-    senti_vari_cat = factor(senti_vari_cat,
-      levels = c("none", levels(nominees$senti_vari_cat)[1:2])
-    )
-  )
+# add cut-offs to label
 levels(noms_all$prevbooks) <- c("none", "medium (1-10)", "many (>10)")
 levels(noms_all$prevprize) <- c("none", "medium (1-7)", "many (>7)")
 
 
 # prep varlist and table options
 varlist <- winner ~ revs_n_cat + senti_mean_cat + senti_vari_cat + wv_mean_cat +
-  prevbooks + prevprize + pub_reputation + nom_prize_prev +
+  books_dnb_prev_cat + wikiprizes_pre_cat + # prevbooks + prevprize +
+  pub_reputation + nom_prize_prev +
   topic_history + topic_politics + topic_relations + topic_identity +
   topic_culture +
   female + homophily + language_german + debut + nonfiction
@@ -33,12 +25,17 @@ tableby(varlist, data = noms_all, total = FALSE, cat.simplify = TRUE) |>
     senti_mean_cat = "Review sentiment",
     senti_vari_cat = "Variance sentiment",
     wv_mean_cat = sprintf(
-      "Wikipedia views %s",
-      levels(noms_all$wv_mean_cat)[2]
+      "Wikipedia views %s", levels(noms_all$wv_mean_cat)[2]
     ),
     pub_reputation = "High Publisher Reputation",
     prevbooks = "# previous books",
+    books_dnb_prev_cat = sprintf(
+      "# previous books %s", levels(noms_all$books_dnb_prev_cat)[2]
+    ),
     prevprize = "# previous prizes",
+    wikiprizes_pre_cat = sprintf(
+      "# previous prizes %s", levels(noms_all$wikiprizes_pre_cat)[2]
+    ),
     nom_prize_prev = "Previously nominated",
     topic_history = "Topic: History",
     topic_politics = "Topic: Politics",
