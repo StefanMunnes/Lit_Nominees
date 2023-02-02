8202d2e647b6339976842cbecc287427f8b9ecbb
Stefan Munnes <munnes@wzb.eu>
2023-02-03 00:15:44 +0100

    shortend var recoding; wv_mean just NA as 0

--- scripts/07_recode_vars.r
+++ scripts/07_recode_vars.r
@@ -11,21 +11,21 @@ nominees_rec <- nominees |>
     ),
     # age as groups
     age_group = case_when(
-      age_nom >= 18 & age_nom <= 42 ~ "18-42",
-      age_nom > 42 & age_nom <= 66 ~ "43-66",
-      age_nom > 66 & age_nom <= 93 ~ "67-92"
+      age_nom < 43 ~ "18-42",
+      between(age_nom, 43, 66) ~ "43-66",
+      age_nom > 66 ~ "67-92"
     ) |> factor(level = c("18-42", "43-66", "67-92")),
     # jury gender share as groups
     jury_group = case_when(
-      jury_fem >= 0.0 & jury_fem <= 0.49 ~ "male",
+      jury_fem < 0.5 ~ "male",
       jury_fem == 0.5 ~ "even",
-      jury_fem >= 0.51 & jury_fem <= 1.0 ~ "female"
+      jury_fem > 0.5 ~ "female"
     ) |> factor(level = c("male", "even", "female")),
     #  reviewers gender share as groups
     revs_group = case_when(
-      revs_fem >= 0.0 & revs_fem <= 0.49 ~ "male",
+      revs_fem < 0.5 ~ "male",
       revs_fem == 0.5 ~ "even",
-      revs_fem >= 0.51 & revs_fem <= 1.0 ~ "female"
+      revs_fem > 0.5 ~ "female"
     ) |> factor(level = c("male", "even", "female")),
     prevprize = case_when(
       wikiprizes_pre == 0 ~ "none",
@@ -36,21 +36,20 @@ nominees_rec <- nominees |>
     prevbooks = case_when(
       books_dnb_prev == 0 ~ "none",
       books_dnb_prev > 0 & books_dnb_prev < 11 ~ "medium",
-      books_dnb_prev >= 11 ~ "many"
+      books_dnb_prev > 10 ~ "many"
     ) |> factor(level = c("none", "medium", "many")),
-    # sentiment variation as
+    # sentiment variation 0 if no variation but valid sentiment values
     senti_vari = ifelse(!is.na(senti_mean) & is.na(senti_vari), 0, senti_vari),
     # gen homophily variable
     homophily = case_when(
-      jury_group == "male" & female == FALSE ~ TRUE,
-      jury_group == "female" & female == TRUE ~ TRUE,
+      jury_group == "male" & !female ~ TRUE,
+      jury_group == "female" & female ~ TRUE,
       TRUE ~ FALSE
     ),
-    # wikiviews
-    wv_mean = ifelse(is.na(wv_mean) | wv_mean == 0, 1, wv_mean),
-    # Calculate wv_mean_log new
-    wv_mean_log = log(wv_mean),
-    wv_mean = ifelse(wv_mean >= 100, 100, wv_mean),
+    # wikiviews NA = 0 & topcoding to erase extrem values
+    wv_mean = ifelse(is.na(wv_mean), 0, wv_mean),
+    wv_mean = ifelse(wv_mean > 100, 100, wv_mean),
+    # nonfiction
     nonfiction = case_when(
       poetry == "Y" ~ TRUE,
       str_detect(tpcs, "Lyrik") ~ TRUE,
@@ -61,13 +60,13 @@ nominees_rec <- nominees |>
     revs_n = ifelse(is.na(revs_n), 0, revs_n),
     # create Zeitgeist variables
     metoo = ifelse(ynom < 2017, "Before #metoo", "After #metoo") |>
-      factor(level = c("Before #metoo", "After #metoo")),
+      as.factor() |> forcats::fct_relevel(rev),
     syria = ifelse(ynom < 2015, "Before 2015", "After 2015") |>
-      factor(level = c("Before 2015", "After 2015")),
+      as.factor() |> forcats::fct_relevel(rev),
     # create two dimensional variables from metric ones
     across(
       c(
-        revs_n, senti_mean, senti_vari, wv_mean, pub_reputation_mean,
+        revs_n, senti_mean, senti_vari, wv_mean,
         wikiprizes_pre, books_dnb_prev
       ),
       ~ case_when(
@@ -79,8 +78,10 @@ nominees_rec <- nominees |>
       ) |>
         as.factor(),
       .names = "{.col}_cat"
-    )
+    ),
+    across(senti_mean_cat:senti_vari_cat, relevel, "none")
   )
+
 
 # Save dataset
 saveRDS(nominees_rec, file = "../data/nominees_rec.RDS")
