4de4143b3328f5abbb46dbdd75a0424805b026d2
Stefan Munnes <munnes@wzb.eu>
2023-02-08 14:43:36 +0100

    add new pub_reputation_mean_cat + lab and winner

--- scripts/08_tab_nominees_vars.R
+++ scripts/08_tab_nominees_vars.R
@@ -3,7 +3,7 @@ p_load("arsenal")
 
 # load data and category of by-varialbe to all observations
 noms_all <- readRDS("../data/nominees_rec.RDS") |>
-  mutate(winner = "All Nominations")
+  mutate(winner_all = "All Nominations")
 
 # add cut-offs to label
 # levels(noms_all$prevbooks) <- c("none", "medium (1-10)", "many (>10)")
@@ -11,45 +11,48 @@ noms_all <- readRDS("../data/nominees_rec.RDS") |>
 
 
 # prep varlist and table options
-varlist <- winner ~ revs_n_cat + senti_qual_cat + wv_mean_cat +
-  # senti_mean_cat + senti_vari_cat +
-  books_dnb_prev_cat + wikiprizes_pre_cat + # prevbooks + prevprize +
-  pub_reputation + nom_prize_prev +
+varlist <- winner_all ~ winner + revs_n_cat + senti_qual_cat +
+  books_dnb_prev_cat + wikiprizes_pre_cat + nom_prize_prev +
+  pub_reputation_mean_cat + wv_mean_cat +
   topic_history + topic_politics + topic_relations + topic_identity +
-  topic_culture +
-  female + homophily + language_german + debut + nonfiction
+  topic_culture + nonfiction +
+  female + homophily + language_german + debut
 
 
 tableby(varlist, data = noms_all, total = FALSE, cat.simplify = TRUE) |>
-  set_labels(list(
-    revs_n_cat = sprintf("# Reviews %s", levels(noms_all$revs_n_cat)[2]),
-    senti_qual_cat = "Review Quality",
-    senti_mean_cat = "Review sentiment",
-    senti_vari_cat = "Variance sentiment",
-    wv_mean_cat = sprintf(
-      "Wikipedia views %s", levels(noms_all$wv_mean_cat)[2]
-    ),
-    pub_reputation = "High Publisher Reputation",
-    prevbooks = "# previous books",
-    books_dnb_prev_cat = sprintf(
-      "# previous books %s", levels(noms_all$books_dnb_prev_cat)[2]
-    ),
-    prevprize = "# previous prizes",
-    wikiprizes_pre_cat = sprintf(
-      "# previous prizes %s", levels(noms_all$wikiprizes_pre_cat)[2]
-    ),
-    nom_prize_prev = "Previously nominated",
-    topic_history = "Topic: History",
-    topic_politics = "Topic: Politics",
-    topic_relations = "Topic: Relations",
-    topic_identity = "Topic: Identity",
-    topic_culture = "Topic: Culture",
-    female = "Female",
-    homophily = "Jury Homophily",
-    language_german = "German Background",
-    debut = "Debut",
-    nonfiction = "Nonfiction"
-  )) |>
+  set_labels(
+    list(
+      winner = "Winner",
+      revs_n_cat = sprintf("# Reviews %s", levels(noms_all$revs_n_cat)[2]),
+      senti_qual_cat = "Review Quality",
+      senti_mean_cat = "Review sentiment",
+      senti_vari_cat = "Variance sentiment",
+      books_dnb_prev_cat = sprintf(
+        "# previous books %s", levels(noms_all$books_dnb_prev_cat)[2]
+      ),
+      wikiprizes_pre_cat = sprintf(
+        "# previous prizes %s", levels(noms_all$wikiprizes_pre_cat)[2]
+      ),
+      nom_prize_prev = "Previously nominated",
+      pub_reputation_mean_cat = sprintf(
+        "High Publisher Reputation %s",
+        levels(noms_all$pub_reputation_mean_cat)[2]
+      ),
+      wv_mean_cat = sprintf(
+        "Wikipedia views %s", levels(noms_all$wv_mean_cat)[2]
+      ),
+      topic_history = "Topic: History",
+      topic_politics = "Topic: Politics",
+      topic_relations = "Topic: Relations",
+      topic_identity = "Topic: Identity",
+      topic_culture = "Topic: Culture",
+      nonfiction = "Nonfiction",
+      female = "Female",
+      homophily = "Jury Homophily",
+      language_german = "German Background",
+      debut = "Debut"
+    )
+  ) |>
   write2word(
     file = "tab_nominees_vars.doc",
     keep.md = FALSE
