46ff79507670339a8edd711ae9529c0216b8da26
Stefan Munnes <munnes@wzb.eu>
2023-06-12 17:08:26 +0200

    new table with SEs and extra model fits

--- scripts/08_an_reg_1_table.R
+++ scripts/08_an_reg_1_table.R
@@ -1,16 +1,13 @@
+# add papge characteristics
 sect_properties <- prop_section(
-  page_size = page_size(
-    orient = "landscape",
-  ),
-  # page_mar(top = NULL),
+  page_size = page_size(orient = "landscape"),
   type = "continuous"
 )
 
-modelsummary(margins_log,
-  # vcov = ~prize, # clustered SE's
-  # stars = c("*" = .05, "**" = .01, "***" = 0.001),
+
+# 1. table: without standard error
+modelsummary(margins_log[1:5],
   coef_map = coef_labs,
-  gof_map = "nobs", # only display N
   estimate = "{estimate}",
   statistic = NULL,
   shape = term ~ model,
@@ -21,6 +18,46 @@ modelsummary(margins_log,
   fontsize(size = 8, part = "all") |>
   padding(padding = 1, part = "all") |>
   save_as_docx(
-    path = "../output/tables/tab_models_log_qual.docx",
+    path = "../output/tables/tab_log_margins.docx",
+    pr_section = sect_properties
+  )
+
+
+# 2. table: with confidence intervall
+modelsummary(margins_log,
+  stars = c("*" = .05, "**" = .01, "***" = 0.001),
+  coef_map = coef_labs,
+  estimate = "{estimate}\n({std.error}){stars}",
+  statistic = NULL,
+  shape = term ~ model,
+  title = "Average Marginal Effects",
+  output = "flextable"
+) |>
+  autofit() |>
+  fontsize(size = 8, part = "all") |>
+  padding(padding = 1, part = "all") |>
+  save_as_docx(
+    path = "../output/tables/tab_log_margins_fe_se.docx",
+    pr_section = sect_properties
+  )
+
+
+
+# 3. table: with goodness of fit statistics to add manualy to margin tables
+gm <- modelsummary::gof_map
+gm$omit <- "FALSE"
+gm$fmt[gm$raw == "r2.tjur"] <- 3
+
+modelsummary(models_log,
+  coef_omit = -1,
+  gof_omit = "Lik|RMSE",
+  gof_map = gm,
+  shape = term ~ model,
+  output = "flextable"
+) |>
+  autofit() |>
+  fontsize(size = 8, part = "all") |>
+  save_as_docx(
+    path = "../output/tables/tab_log_margins_gof.docx",
     pr_section = sect_properties
   )
